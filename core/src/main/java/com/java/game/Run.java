462af6f1b989b8bd16e54ab36fafea600348f661
Gigamoose <boardedmind@gmail.com>
2026-06-03 22:25:15 +0800

    Added circle funcctionalities

--- core/src/main/java/com/java/game/Run.java
+++ core/src/main/java/com/java/game/Run.java
@@ -1,32 +1,47 @@
 package com.java.game;
 
+import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Run extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    private ShapeRenderer shape;
+    private int cx, cy, cr;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        shape = new ShapeRenderer();
+        cx = Gdx.graphics.getWidth()/2;
+        cy = Gdx.graphics.getHeight()/2;
+        cr = 50;
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();        
-        batch.draw(image, 140, 210);
-        batch.end();
+        ScreenUtils.clear(0.051f, 0.075f, 0.067f, 1.0f);
+
+        // animations
+
+        // renders a circle
+        shape.begin(ShapeType.Filled);
+        if (!UIManager.get().isHoveredCircle(cx, cy, cr)) {
+            shape.setColor(0.957f, 0.945f, 0.918f, 1.0f);
+        } else {
+            shape.setColor(0.322f, 0.718f, 0.533f, 1.0f);
+        }
+        shape.circle(cx, cy, cr);
+        shape.end();
+
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        shape.dispose();
     }
 }
--- core/src/main/java/com/java/game/UIManager.java
+++ core/src/main/java/com/java/game/UIManager.java
@@ -17,15 +17,25 @@ public class UIManager {
         return instance;
     }
     
-    boolean isHovered(int x, int y, int width, int height) {
+    public boolean isHoveredRect(int x, int y, int width, int height) {
         mx = Gdx.input.getX();
         my = Gdx.graphics.getHeight() - Gdx.input.getY();
-
         return x <= mx && x + width >= mx && y <= my && y + height >= my;
     }
 
-    boolean isClicked(int x, int y, int width, int height) {
+    public boolean isClickedRect(int x, int y, int width, int height) {
+        return isHoveredRect(x, y, width, height) && Gdx.input.isButtonJustPressed(Input.Buttons.LEFT);
+    }
 
-        return isHovered(x, y, width, height) && Gdx.input.isButtonJustPressed(Input.Buttons.LEFT);
+    public boolean isHoveredCircle(int x, int y, int r) {
+        mx = Gdx.input.getX();
+        my = Gdx.graphics.getHeight() - Gdx.input.getY();
+        float dx = mx - x;
+        float dy = my - y;
+        return (dx * dx + dy * dy) <= (r * r);
+    }
+
+    public boolean isClickedCircle(int x, int y, int r) {
+        return isHoveredCircle(x, y, r) && Gdx.input.isButtonJustPressed(Input.Buttons.LEFT);
     }
 }
